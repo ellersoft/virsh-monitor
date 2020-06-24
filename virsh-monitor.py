7ff66973b7c175f3d96f4ee4ef74328e7126593c
Elliott Brown <elliott.brown@ellersoft.com>
2020-06-24 17:18:14 -0400

    More cleanup and use the `libvirt` python module

--- virsh-monitor.py
+++ virsh-monitor.py
@@ -1,10 +1,11 @@
 #!/bin/python3
 
 
-import subprocess
-import re
 import curses
 import enum
+import libvirt
+import getopt
+import sys
 
 
 class Colors(enum.IntFlag):
@@ -32,6 +33,56 @@ class Colors(enum.IntFlag):
 		curses.init_pair(Colors.HEAD, sel_head_fg, head_bg)
 		curses.init_pair(Colors.GRAY, gray_fg, -1)
 		curses.init_pair(Colors.GRAY | Colors.SELECT, gray_fg, select_bg)
+
+
+class State:
+	# noinspection SpellCheckingInspection
+	state_dict = {
+		libvirt.VIR_DOMAIN_NOSTATE: ('no state', 0),
+		libvirt.VIR_DOMAIN_RUNNING: ('running', 1),
+		libvirt.VIR_DOMAIN_BLOCKED: ('blocked', 3),
+		libvirt.VIR_DOMAIN_PAUSED: ('paused', 4),
+		libvirt.VIR_DOMAIN_SHUTDOWN: ('shut down', 6),
+		libvirt.VIR_DOMAIN_SHUTOFF: ('shut off', 7),
+		libvirt.VIR_DOMAIN_CRASHED: ('crashed', 2),
+		libvirt.VIR_DOMAIN_PMSUSPENDED: ('suspended', 5),
+	}
+	active_dict = {
+		0: ('inactive', 1),
+		1: ('active', 0),
+	}
+
+	@staticmethod
+	def state_val(item):
+		return State.state_dict[item][0]
+
+	@staticmethod
+	def state(item):
+		return State.state_val(item.state()[0])
+
+	@staticmethod
+	def sort(val):
+		return State.state_dict[val][1]
+
+	@staticmethod
+	def active_val(item):
+		return State.active_dict[item][0]
+
+	@staticmethod
+	def active(item):
+		return State.active_val(item.isActive())
+
+	@staticmethod
+	def set_x_for_true(x):
+		return 'X' if x else ' '
+
+	@staticmethod
+	def auto_start(item):
+		return State.set_x_for_true(item.autostart())
+
+	@staticmethod
+	def persistent(item):
+		return State.set_x_for_true(item.isPersistent())
 
 
 def print_table(std_scr, sel_i, x, y, cols, gray_sel, items):
@@ -64,14 +115,10 @@ def print_help(std_scr, help_color, helps):
 		offset += len(key) + max_len
 
 
-def set_x_for_yes(x):
-	return 'X' if x == 'yes' else ' '
-
-
 def render(std_scr, vms, nets, pools, sel, sel_i):
 	pool_diff = 2
-	longest_net = max(len(net[0]) for net in nets)
-	longest_pool = max(len(pool[0]) for pool in pools)
+	longest_net = max((len(net.name()) for net in nets), default=0)
+	longest_pool = max((len(pool.name()) for pool in pools), default=0)
 	longest_net = max(longest_net, longest_pool - pool_diff)
 	height, width = std_scr.getmaxyx()
 	net_offset = width - longest_net - 9 - pool_diff - 3
@@ -81,13 +128,15 @@ def render(std_scr, vms, nets, pools, sel, sel_i):
 	vm_table = [("ID", 3, False), ("VM", vm_width - 1, True), ("STATUS", 9, False)]
 	net_table = [("NET", longest_net, True), ("STATUS", 8, False), ("A", 1, False), ("P", 1, False)]
 	pool_table = [("POOL", longest_net + pool_diff, True), ("STATUS", 8, False), ("A", 1, False)]
-	nets = [[net[0], net[1], set_x_for_yes(net[2]), set_x_for_yes(net[3])] for net in nets]
-	pools = [[pool[0], pool[1], set_x_for_yes(pool[2])] for pool in pools]
+
+	vms = [['-' if vm.ID() == -1 else str(vm.ID()), vm.name(), State.state(vm)] for vm in vms]
+	nets = [[net.name(), State.active(net), State.auto_start(net), State.persistent(net)] for net in nets]
+	pools = [[pool.name(), State.active(pool), State.auto_start(pool)] for pool in pools]
 
 	tables = [
-		(0, 0, 0, vm_table, lambda vm: vm[2] != "running", vms),
-		(1, net_offset, 0, net_table, lambda net: net[1] != "active", nets),
-		(2, net_offset, len(nets) + 2, pool_table, lambda pool: pool[1] != "active", pools)
+		(0, 0, 0, vm_table, lambda vm: vm[2] != State.state_val(libvirt.VIR_DOMAIN_RUNNING), vms),
+		(1, net_offset, 0, net_table, lambda net: net[1] != State.active_val(1), nets),
+		(2, net_offset, len(nets) + 2, pool_table, lambda pool: pool[1] != State.active_val(1), pools)
 	]
 
 	for sel_c, x, y, table, sel_test, items in tables:
@@ -97,29 +146,19 @@ def render(std_scr, vms, nets, pools, sel, sel_i):
 	print_help(std_scr, curses.color_pair(Colors.DEFAULT | Colors.SELECT), helps)
 
 
-def virsh(*args):
-	out = subprocess.check_output(('virsh', *args))
-	out = re.split('[\r\n]+', out.decode("utf-8"))
-	return [[x.strip() for x in re.split('\\s{2,}', line)] for line in out]
-
-
-def main(std_scr):
+def main(std_scr, con):
 	curses.curs_set(0)
 	curses.halfdelay(20)
 	Colors.init_curses()
 	sel = 0
 	sel_i = 0
 
-	start_commands = ['start', 'net-start', 'pool-start']
-	stop_commands = ['destroy', 'net-destroy', 'pool-destroy']
-	arg_indexes = [1, 0, 0]
-
 	while True:
-		vms = virsh('list', '--all')[2:][:-1]
-		nets = virsh('net-list', '--all')[2:][:-1]
-		pools = virsh('pool-list', '--all')[2:][:-1]
+		vms = sorted(con.listAllDomains(), key=lambda vm: (State.sort(vm.state()[0]), vm.ID()))
+		nets = sorted(con.listAllNetworks(), key=lambda net: (State.active_dict[net.isActive()][1], net.name()))
+		pools = sorted(con.listAllStoragePools(), key=lambda pool: (State.active_dict[pool.isActive()][1], pool.name()))
 
-		args = [vms, nets, pools]
+		arg = [vms, nets, pools]
 
 		std_scr.clear()
 		render(std_scr, vms, nets, pools, sel, sel_i)
@@ -132,15 +171,39 @@ def main(std_scr):
 			sel = 0 if sel == 2 else sel + 1
 		elif c == curses.KEY_DOWN or c == curses.KEY_UP:
 			sel_i += -1 if c == curses.KEY_UP else 1
-		elif (c == curses.KEY_F1 or c == curses.KEY_F2) and sel_i < len(args[sel]):
-			commands = stop_commands if c == curses.KEY_F2 else start_commands
-			virsh(commands[sel], args[sel][sel_i][arg_indexes[sel]])
+		elif (c == curses.KEY_F1 or c == curses.KEY_F2) and sel_i < len(arg[sel]):
+			try:
+				if c == curses.KEY_F1:
+					# noinspection PyUnresolvedReferences
+					arg[sel][sel_i].create()
+				else:
+					# noinspection PyUnresolvedReferences
+					arg[sel][sel_i].destroy()
+			except libvirt.libvirtError:
+				pass
 
 		if sel_i == -1:
 			sel_i += 1
-		if sel_i >= len(args[sel]):
-			sel_i = len(args[sel]) - 1
+		if sel_i >= len(arg[sel]):
+			sel_i = len(arg[sel]) - 1
 
 
 if __name__ == '__main__':
-	curses.wrapper(main)
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], 'c:')
+	except getopt.GetoptError as err:
+		print(err)
+		sys.exit(1)
+
+	uri = None
+	for o, a in opts:
+		if o == '-c':
+			uri = a
+
+	try:
+		conn = libvirt.open(uri)
+	except libvirt.libvirtError as err:
+		print('Failed to open connection to the hypervisor')
+		sys.exit(1)
+
+	curses.wrapper(main, conn)
